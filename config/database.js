739ca5951e39180deac7e19221253a87ae0e1b3c
lagzda <ralfs.lagzda@gmail.com>
2017-11-11 22:38:32 +0000

    dsad

--- config/database.js
+++ config/database.js
@@ -1,6 +1,4 @@
 // config/database.js
 module.exports = {
-
-	'url' : 'mongodb://localhost/messenger-bank' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
+	'url' : 'mongodb://heroku_r1sbvqhh:ag5ct7kshbkr99qlr79estbh05@ds157475.mlab.com:57475/heroku_r1sbvqhh' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
